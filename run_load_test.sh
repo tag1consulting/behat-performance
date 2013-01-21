6d415525a204f7e79728f7fd62d91b18da1479b3
catch <catch@35733.no-reply.drupal.org>
2013-01-21 11:36:03 +0000

    Behat doesn't like being run from outside the tests directory, make this work.

--- run_load_test.sh
+++ run_load_test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-source run_load_test.cfg
-./start_sahi.sh&
+
+DIR=$(cd $(dirname "$0"); pwd)
+source $DIR/run_load_test.cfg
+$DIR/start_sahi.sh&
 PID=$!
 $DRUSH -r $RESULTS_SITE_PATH -l $RESULTS_SITE_URI pts
-$BEHAT $FEATURES
+
+# Behat fails to find the bootstrap directory unless run from the behat
+# install so change directory here.
+cd $BEHAT_TESTS
+$BEHAT_TESTS/bin/behat $BEHAT_TESTS/features
 $DRUSH -r $RESULTS_SITE_PATH -l $RESULTS_SITE_URI pttd
 pkill -P $PID
