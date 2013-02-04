2bc3f0711a00e52160f2b6b4d0d16429634ba1a7
catch <catch@35733.no-reply.drupal.org>
2013-02-04 16:50:54 +0000

    Fix pwd.

--- start_sahi.sh
+++ start_sahi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-source run_load_test.cfg
+DIR=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
+source $DIR/run_load_test.cfg
 
 export SAHI_USERDATA_DIR=$SAHI_HOME/userdata
 export SAHI_EXT_CLASS_PATH=
